94a759ac3763c53760d3bbe82e51eb2da4db2ca9
kangddong <rkdehddud96@naver.com>
2022-06-22 21:51:20 +0900

    button Image

---
+++ buttonImageInset/buttonImageInset/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  buttonImageInset
+//
+//  Created by 강동영 on 2022/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        
+        
+    }
+
+
+}
+
